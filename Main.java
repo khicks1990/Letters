6648500c72bb61b68098b9dde8d26b90e2113570
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-19 08:53:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+// This program demonstrates the close relationship between
+// characters and integers.
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      char letter;
+
+      letter = 'A';
+      System.out.println(letter);
+      letter = 66;
+      System.out.println(letter);
+   }
+}
